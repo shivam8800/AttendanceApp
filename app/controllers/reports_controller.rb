845f288bbfadc578f41786d0c17a0743bc2f4713
shivam8800 <shivam16@navgurukul.org>
2018-02-22 13:52:47 +0530

    complete attendence show page

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -7,10 +7,7 @@ class ReportsController < ApplicationController
     end
 
     def create
-        day, month, year = params["report"]["date(3i)"].to_i, params["report"]["date(2i)"].to_i, params["report"]["date(1i)"].to_i
-        date = Date.new(year,month,day)
-        subject_id = params["report"]["subject_id"].to_i
-
+        reports_params = params.require(:report).permit(:date, :subject_id)
         @report = Report.new(reports_params)
         if @report.save
             attendences_params["attendences_attributes"].each do |attendence, value|
@@ -21,16 +18,21 @@ class ReportsController < ApplicationController
                 if @attendence.save
                 end
             end
-            redirect_to subject_path(@report.subject_id)            
+            redirect_to report_path(@report)
         else 
             render "new"
         end
     end
 
+    def show
+        @report = Report.find(params[:id])
+        @attendences = @report.attendences
+    end
+
     private
-        def reports_params
-            params.require(:report).permit(:date, :subject_id)
-        end
+        # def reports_params
+        #     params.require(:report).permit(:date, :subject_id)
+        # end
 
         def attendences_params
             params.require(:report).permit(attendences_attributes: [:student_id, :present])
